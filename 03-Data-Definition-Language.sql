e8c56b0edc9e2b5869157061526619899695baa6
bioroid17 <trace1997@ajou.ac.kr>
2025-05-03 01:08:58 +0900

    Data Manipulation Language - Introduction

--- 03-Data-Definition-Language.sql
+++ 03-Data-Definition-Language.sql
@@ -4,15 +4,20 @@ CREATE TABLE movies (
 	movie_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	title TEXT UNIQUE NOT NULL,
 	released INTEGER NOT NULL CHECK (released > 0), -- 1, 2, 3, ...
-	overview TEXT NOT NULL CHECK (LENGTH(overview) <= 100),
+	overview TEXT NOT NULL CHECK (LENGTH (overview) <= 100),
 	rating REAL NOT NULL CHECK (rating BETWEEN 0 AND 10), -- 1.2, 9.7, ...
 	director TEXT,
 	for_kids INTEGER NOT NULL DEFAULT 0 CHECK (for_kids BETWEEN 0 AND 1) -- 0 or 1
--- 	poster BLOB
+	-- 	poster BLOB
 ) STRICT;
 
 INSERT INTO
-  movies (title, rating, released, overview)
+	movies (title, rating, released, overview)
 VALUES
-  ('The Lord Of The Rings', 0.5, 1, 'Rings and hobbits'),
-  ('Dune: Part One', 10, 1, 'Sand');
+	(
+		'The Lord Of The Rings',
+		0.5,
+		1,
+		'Rings and hobbits'
+	),
+	('Dune: Part One', 10, 1, 'Sand');
---
+++ 04-Data-Manipulation-Language.sql
@@ -0,0 +1,3 @@
+-- Data Manipulation Language (DML)
+
+-- Two categories: 'update commands' and 'query command'
