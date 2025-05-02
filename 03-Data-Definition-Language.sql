fba3d1948b7ca76bcf2e8ba2e8bb168ae19ee148
bioroid17 <trace1997@ajou.ac.kr>
2025-05-02 23:57:36 +0900

    Data Definition Language - Recap

--- 03-Data-Definition-Language.sql
+++ 03-Data-Definition-Language.sql
@@ -3,7 +3,7 @@ DROP TABLE movies;
 CREATE TABLE movies (
 	title TEXT UNIQUE NOT NULL,
 	released INTEGER NOT NULL CHECK (released > 0), -- 1, 2, 3, ...
-	overview TEXT NOT NULL,
+	overview TEXT NOT NULL CHECK (LENGTH(overview) <= 100),
 	rating REAL NOT NULL CHECK (rating BETWEEN 0 AND 10), -- 1.2, 9.7, ...
 	director TEXT,
 	for_kids INTEGER NOT NULL DEFAULT 0 CHECK (for_kids BETWEEN 0 AND 1) -- 0 or 1
@@ -13,5 +13,5 @@ CREATE TABLE movies (
 INSERT INTO
   movies (title, rating, released, overview, for_kids)
 VALUES
-  ('The Lord Of The Rings', 0.5, 1, 'Rings and hobbits', 0),
+  ('The Lord Of The Rings', 0.5, 1, 'Rings and hobbits qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm qazwsxedc rfv tgb', 0),
   ('Dune: Part One', 10, 1, 'Sand', 1);
