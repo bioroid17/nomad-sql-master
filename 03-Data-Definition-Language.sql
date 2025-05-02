e0c5b6861eca28a163b15ae2506068b00c034662
bioroid17 <trace1997@ajou.ac.kr>
2025-05-02 23:56:31 +0900

    Data Definition Language - CHECK Constraint

--- 03-Data-Definition-Language.sql
+++ 03-Data-Definition-Language.sql
@@ -2,16 +2,16 @@ DROP TABLE movies;
 
 CREATE TABLE movies (
 	title TEXT UNIQUE NOT NULL,
-	released INTEGER NOT NULL, -- 1, 2, 3, ...
+	released INTEGER NOT NULL CHECK (released > 0), -- 1, 2, 3, ...
 	overview TEXT NOT NULL,
-	rating REAL NOT NULL, -- 1.2, 9.7, ...
+	rating REAL NOT NULL CHECK (rating BETWEEN 0 AND 10), -- 1.2, 9.7, ...
 	director TEXT,
-	for_kids INTEGER NOT NULL DEFAULT 0 -- 0 or 1
+	for_kids INTEGER NOT NULL DEFAULT 0 CHECK (for_kids BETWEEN 0 AND 1) -- 0 or 1
 -- 	poster BLOB
 ) STRICT;
 
 INSERT INTO
   movies (title, rating, released, overview, for_kids)
 VALUES
-  ('The Lord Of The Rings', -10, -100, 'Rings and hobbits', 121212),
-  ('Dune: Part One', -10, -100, 'Sand',123);
+  ('The Lord Of The Rings', 0.5, 1, 'Rings and hobbits', 0),
+  ('Dune: Part One', 10, 1, 'Sand', 1);
