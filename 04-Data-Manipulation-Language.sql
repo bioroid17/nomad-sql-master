bf82a2ca6066066bf569cc29fd99f9e25e914b03
bioroid17 <trace1997@ajou.ac.kr>
2025-05-03 02:36:42 +0900

    Data Manipulation Language - Update Commands

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,3 +1,15 @@
--- Data Manipulation Language (DML)
+-- INSERT INTO VALUES
 
--- Two categories: 'update commands' and 'query command'
+INSERT INTO
+	movies (title, rating, released, overview)
+VALUES
+-- 	('The Lord Of The Rings', 0.5, 1, 'Rings and hobbits'),
+	('Dune: Part One', 10, 1, 'Sand');
+
+-- UPDATE
+
+UPDATE movies SET director = 'Unknown' WHERE director IS NULL AND rating = 10;
+
+-- DELETE
+
+DELETE FROM movies WHERE movie_id = 4;
