ca7b33f7aa37760107cc65b3ad7952991fa885d0
bioroid17 <trace1997@ajou.ac.kr>
2025-05-03 20:02:52 +0900

    Data Manipulation Language - SELECT Commands

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,15 +1,1 @@
--- INSERT INTO VALUES
-
-INSERT INTO
-	movies (title, rating, released, overview)
-VALUES
--- 	('The Lord Of The Rings', 0.5, 1, 'Rings and hobbits'),
-	('Dune: Part One', 10, 1, 'Sand');
-
--- UPDATE
-
-UPDATE movies SET director = 'Unknown' WHERE director IS NULL AND rating = 10;
-
--- DELETE
-
-DELETE FROM movies WHERE movie_id = 4;
+SELECT 1+1, 2+2, UPPER('hello');
