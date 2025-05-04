2960fbb7429a289385651063c08c702e3ce430ea
bioroid17 <trace1997@ajou.ac.kr>
2025-05-05 02:56:10 +0900

    Data Manipulation Language - SELECT Expressions

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,1 +1,6 @@
-SELECT title, rating FROM movies;
+SELECT
+    REPLACE(title, ': Part One', 'I') AS title,
+    rating * 2 AS double_rating,
+    UPPER(overview) AS overview_uppercase
+FROM
+    movies;
