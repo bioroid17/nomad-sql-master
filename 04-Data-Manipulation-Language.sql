bea2536eca719a928da45c3aff978dfed1c985eb
bioroid17 <trace1997@ajou.ac.kr>
2025-05-08 01:09:09 +0900

    Data Manipulation Language - WHERE Predicates

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,10 +1,79 @@
+-- Range condition
 SELECT
   *
 FROM
   movies
 WHERE
-  rating > 9
-  OR (
-    rating IS NULL
-    AND genres = 'Documentary'
-  );
+  release_date BETWEEN 2020 AND 2024;
+
+-- Membership condition
+SELECT
+  *
+FROM
+  movies
+WHERE
+  genres IN ('Documentary', 'Comedy');
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  original_language NOT IN ('en', 'es', 'de');
+
+-- Pattern matching
+SELECT
+  *
+FROM
+  movies
+WHERE
+  title LIKE 'The%';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  title LIKE '%Love';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  overview LIKE '%kimchi%';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  genres NOT LIKE '%Drama%';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  title LIKE '___ing';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  title LIKE 'The __';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  director NOT LIKE '_____ Roberts';
+
+SELECT
+  *
+FROM
+  movies
+WHERE
+  title LIKE 'The ___ %';
