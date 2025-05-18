676a450a9eacd136404e4b49f1954222ec5307d1
bioroid17 <trace1997@ajou.ac.kr>
2025-05-18 10:51:16 +0900

    Data Manipulation Language - SELECT CASE

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,79 +1,9 @@
--- Range condition
 SELECT
-  *
+  title,
+  CASE
+    WHEN rating >= 8 THEN '👍'
+    WHEN rating <= 6 THEN '👎'
+    ELSE '👀'
+  END AS good_or_not
 FROM
-  movies
-WHERE
-  release_date BETWEEN 2020 AND 2024;
-
--- Membership condition
-SELECT
-  *
-FROM
-  movies
-WHERE
-  genres IN ('Documentary', 'Comedy');
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  original_language NOT IN ('en', 'es', 'de');
-
--- Pattern matching
-SELECT
-  *
-FROM
-  movies
-WHERE
-  title LIKE 'The%';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  title LIKE '%Love';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  overview LIKE '%kimchi%';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  genres NOT LIKE '%Drama%';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  title LIKE '___ing';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  title LIKE 'The __';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  director NOT LIKE '_____ Roberts';
-
-SELECT
-  *
-FROM
-  movies
-WHERE
-  title LIKE 'The ___ %';
+  movies;
