848fb5c68866312191215ee81401b21d330f7d99
bioroid17 <trace1997@ajou.ac.kr>
2025-05-19 22:27:44 +0900

    Data Manipulation Language - GROUP BY Clause

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,8 +1,12 @@
-SELECT		-- 3
-  *
-FROM		-- 1
+SELECT	-- 4
+  release_date,
+  ROUND(AVG(rating), 2) AS avg_rating
+FROM	-- 1
   movies
--- WHERE 	-- 2
--- ORDER BY	-- 4
-LIMIT 5		-- 6
-OFFSET 10;	-- 5
+WHERE	-- 2
+  rating IS NOT NULL
+  AND release_date IS NOT NULL
+GROUP BY-- 3
+  release_date
+ORDER BY-- 5
+  avg_rating DESC;
