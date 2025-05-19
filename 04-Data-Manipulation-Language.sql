fc6891804faf6ab8337d3c226b7f28dbbbf1d798
bioroid17 <trace1997@ajou.ac.kr>
2025-05-20 04:43:06 +0900

    Data Manipulation Language - Super Practice Part 1

--- 04-Data-Manipulation-Language.sql
+++ 04-Data-Manipulation-Language.sql
@@ -1,40 +1,56 @@
--- GROUP BY를 실행하고 집계 함수를 쓰지 않는 경우
-SELECT	-- 4
-  release_date
-FROM	-- 1
+-- 1. 각 감독의 평균 평점은?
+SELECT
+  director,
+  AVG(rating) avg_rating
+FROM
   movies
-WHERE	-- 2
-  rating IS NOT NULL
-  AND release_date IS NOT NULL
-GROUP BY-- 3
-  release_date;
-
-
--- GROUP BY를 하지 않은 열을 SELECT 하는 경우
-SELECT -- 4
-  title,
-  release_date,
-  ROUND(AVG(rating), 2) AS avg_rating
-FROM -- 1
+WHERE
+  director IS NOT NULL
+  AND rating IS NOT NULL
+GROUP BY
+  director
+ORDER BY
+  avg_rating DESC;
+	
+-- 2. 5편 이상의 영화를 가진 각 감독의 평균 평점은?
+SELECT
+  director,
+  ROUND(AVG(rating), 3) avg_rating
+FROM
   movies
-WHERE -- 2
-  rating IS NOT NULL
-  AND release_date IS NOT NULL
-GROUP BY -- 3
-  release_date
-ORDER BY -- 5
+WHERE
+  director IS NOT NULL
+  AND rating IS NOT NULL
+GROUP BY
+  director
+HAVING
+  COUNT(*) >= 5
+ORDER BY
   avg_rating DESC;
 
-
--- GROUP BY를 하지 않고 집계 함수를 쓰는 경우
-SELECT -- 4
-  ROUND(SUM(revenue), 2) 
-FROM -- 1
+-- 3. 각 장르에 몇 편의 영화가 있나요?
+SELECT
+  genres,
+  COUNT(*) total_movies
+FROM
   movies
-WHERE -- 2
-  rating IS NOT NULL
-  AND release_date IS NOT NULL
--- GROUP BY -- 3
---   release_date
--- ORDER BY -- 5
---   avg_rating DESC;
+WHERE
+  genres IS NOT NULL
+GROUP BY
+  genres
+ORDER BY
+  total_movies DESC;
+
+-- 4. 평점이 6점보다 높은 영화는 몇 편인가요? 이 중 가장 많은 평점은 몇 점인가요?
+SELECT
+  rating,
+  COUNT(*) AS total_movies
+FROM
+  movies
+WHERE
+  rating > 6
+GROUP BY
+  rating
+ORDER BY
+  total_movies DESC,
+  rating DESC;
