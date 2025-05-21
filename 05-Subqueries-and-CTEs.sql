0e0eef7598cc7c0b3792eb2ba26e30579bdb3a79
bioroid17 <trace1997@ajou.ac.kr>
2025-05-22 03:06:40 +0900

    Subqueries and CTEs - CTEs

--- 05-Subqueries-and-CTEs.sql
+++ 05-Subqueries-and-CTEs.sql
@@ -1,12 +1,26 @@
--- 전체 영화 중 평점이나 수익이 평균 평점 또는 수익보다 높은 영화들의 목록
+-- 전체 영화 중 평점이나 수익이 평균 평점 또는 수익보다 높은 영화들의 제목, 감독, 수익 목록
+WITH
+  avg_revenue_cte AS (
+    SELECT
+      AVG(revenue) AS avg_revenue
+    FROM
+      movies
+  ),
+  avg_rating_cte AS (
+    SELECT
+      AVG(rating) AS avg_rating
+    FROM
+      movies
+  )
 SELECT
-  COUNT(*)
+  title,
+  director,
+  revenue,
+  ROUND( ( SELECT avg_revenue FROM avg_revenue_cte ), 0 ) AS avg_revenue,
+  rating,
+  ROUND( ( SELECT avg_rating FROM avg_rating_cte ), 0 ) AS avg_rating
 FROM
   movies
 WHERE
-  rating > (
-    SELECT
-      AVG(rating)
-    FROM
-      movies
-  );
+  revenue > ( SELECT * FROM avg_revenue_cte )
+  AND rating > ( SELECT * FROM avg_rating_cte );
