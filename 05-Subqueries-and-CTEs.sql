a0aa9ddb1e5deb5dd17396d84a713ee7f5cadbea
bioroid17 <trace1997@ajou.ac.kr>
2025-05-23 02:45:37 +0900

    Subqueries and CTEs - Subquery Practice

--- 05-Subqueries-and-CTEs.sql
+++ 05-Subqueries-and-CTEs.sql
@@ -1,21 +1,78 @@
--- 같은 해에 상영된 영화들의 평균 평점보다 높은 평점을 기록한 영화들의 목록
+-- 자신의 평점이 자신이 속한 장르 평균 평점보다 높은 영화들의 목록
 WITH
-  movie_avg_per_year AS (
+  avg_genre_rating_cte AS (
     SELECT
-      AVG(inner_movies.rating)
+      AVG(sub_movies.rating)
     FROM
-      movies AS inner_movies
+      movies AS sub_movies
     WHERE
-      inner_movies.release_date = main_movies.release_date
+      sub_movies.genres IS NOT NULL
+      AND sub_movies.genres = main_movies.genres
+    GROUP BY
+      sub_movies.genres
   )
 SELECT
   main_movies.title,
-  main_movies.director,
+  main_movies.genres,
   main_movies.rating,
-  main_movies.release_date,
-  ( SELECT*FROM movie_avg_per_year ) AS year_average
+  (
+    SELECT
+      *
+    FROM
+      avg_genre_rating_cte
+  ) as avg_rating
 FROM
   movies AS main_movies
 WHERE
-  main_movies.rating > ( SELECT*FROM movie_avg_per_year )
-  AND release_date > 2020;
+  main_movies.rating > (
+    SELECT
+      *
+    FROM
+      avg_genre_rating_cte
+  )
+  AND main_movies.release_date > 2020;
+
+
+-- 모든 감독들의 평균 수익보다 높은 커리어 수익을 기록한 감독들의 목록
+WITH
+  directors_revenues AS (
+    SELECT
+      director,
+      SUM(revenue) AS career_revenue
+    FROM
+      movies
+    WHERE
+      director IS NOT NULL
+      AND revenue IS NOT NULL
+    GROUP BY
+      director
+  ),
+  avg_director_career_revenue AS (
+    SELECT
+      AVG(career_revenue)
+    FROM
+      directors_revenues
+  )
+SELECT
+  director,
+  SUM(revenue) AS total_revenue,
+  (
+    SELECT
+      *
+    FROM
+      avg_director_career_revenue
+  ) AS peers_avg
+FROM
+  movies
+WHERE
+  director IS NOT NULL
+  AND revenue IS NOT NULL
+GROUP BY
+  director
+HAVING
+  total_revenue > (
+    SELECT
+      *
+    FROM
+      avg_director_career_revenue
+  );
