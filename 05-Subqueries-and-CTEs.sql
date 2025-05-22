fd8cb4f79214e5aecf2c0448313f210fdbe6b24b
bioroid17 <trace1997@ajou.ac.kr>
2025-05-22 23:40:51 +0900

    Subqueries and CTEs - Correlated CTEs

--- 05-Subqueries-and-CTEs.sql
+++ 05-Subqueries-and-CTEs.sql
@@ -1,26 +1,21 @@
 -- 같은 해에 상영된 영화들의 평균 평점보다 높은 평점을 기록한 영화들의 목록
+WITH
+  movie_avg_per_year AS (
+    SELECT
+      AVG(inner_movies.rating)
+    FROM
+      movies AS inner_movies
+    WHERE
+      inner_movies.release_date = main_movies.release_date
+  )
 SELECT
   main_movies.title,
   main_movies.director,
   main_movies.rating,
   main_movies.release_date,
-  (
-    SELECT
-      AVG(inner_movies.rating)
-    FROM
-      movies AS inner_movies
-    WHERE
-      inner_movies.release_date = main_movies.release_date
-  ) AS year_average
+  ( SELECT*FROM movie_avg_per_year ) AS year_average
 FROM
   movies AS main_movies
 WHERE
-  main_movies.rating > (
-    SELECT
-      AVG(inner_movies.rating)
-    FROM
-      movies AS inner_movies
-    WHERE
-      inner_movies.release_date = main_movies.release_date
-  );
---   AND release_date > 2020;
+  main_movies.rating > ( SELECT*FROM movie_avg_per_year )
+  AND release_date > 2020;
