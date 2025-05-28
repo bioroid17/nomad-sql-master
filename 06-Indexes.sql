ef2f56406ffa613fe7cc292e8dc2faa2a9293ee7
bioroid17 <trace1997@ajou.ac.kr>
2025-05-28 20:55:49 +0900

    Indexes - Indexes and Keys

--- 06-Indexes.sql
+++ 06-Indexes.sql
@@ -1,5 +1,7 @@
 SELECT
-  title
+  title,
+  movie_id,
+  rowid
 FROM
   movies
 WHERE
@@ -8,3 +10,19 @@ WHERE
 CREATE INDEX idx_director ON movies (director);
 
 DROP INDEX idx_director;
+
+
+CREATE TABLE X (name TEXT);
+
+INSERT INTO X VALUES ('hello');
+
+SELECT name, rowid FROM X;
+
+SELECT * FROM X;
+
+SELECT *, rowid FROM X;
+
+
+CREATE TABLE Z (name UNIQUE);
+
+INSERT INTO Z VALUES ('hello');
