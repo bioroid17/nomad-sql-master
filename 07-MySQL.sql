8514b7b14c0184146943f7794fd3c9dc0fb45088
bioroid17 <trace1997@ajou.ac.kr>
2025-06-01 00:41:03 +0900

    MySQL - ALTER TABLE part 2

--- 07-MySQL.sql
+++ 07-MySQL.sql
@@ -1,38 +1,11 @@
--- 열 삭제
-ALTER TABLE users DROP COLUMN profile_picture;
+ALTER TABLE users MODIFY COLUMN graduation_year DATE;
 
--- 열 이름 변경(CHANGE COLUMN)
-ALTER TABLE users CHANGE COLUMN bio about_me TINYTEXT;
+ALTER TABLE users ADD COLUMN graduation_date DATE;
 
--- 열 데이터 타입 변경(CHANGE COLUMN)
-ALTER TABLE users CHANGE COLUMN about_me about_me TEXT;
+UPDATE users SET graduation_date = MAKEDATE(graduation_year, 1);
 
--- 열 데이터 타입 변경(MODIFY COLUMN)
-ALTER TABLE users MODIFY COLUMN about_me TINYTEXT;
+ALTER TABLE users DROP COLUMN graduation_year;
 
--- DB 이름 변경
-ALTER TABLE users RENAME TO customers;
+ALTER TABLE users MODIFY COLUMN graduation_date DATE NOT NULL;
 
-ALTER TABLE customers RENAME TO users;
-
--- 제약 조건 삭제
-ALTER TABLE users DROP CONSTRAINT uq_email;
-
-ALTER TABLE users
-DROP CONSTRAINT username,
-DROP CONSTRAINT chk_age;
-
--- 제약 조건 추가
-ALTER TABLE users
-ADD CONSTRAINT uq_email UNIQUE (email),
-ADD CONSTRAINT uq_username UNIQUE (username);
-
-ALTER TABLE users ADD CONSTRAINT chk_age CHECK (age < 100);
-
--- NOT NULL 조건 삭제
-ALTER TABLE users MODIFY COLUMN bed_time TIME NULL;
-
--- NOT NULL 조건 추가
-ALTER TABLE users MODIFY COLUMN bed_time TIME NOT NULL;
-
-SHOW CREATE TABLE users;
+-- ALTER TABLE users ADD COLUMN graduation_date DATE NOT NULL DEFAULT MAKEDATE(graduation_year, 1); -- DEFAULT 값이 상수가 아니므로 에러가 난다.
