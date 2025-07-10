bebe58f0ff19feb21c0f10317a3028d22f78a949
bioroid17 <trace1997@ajou.ac.kr>
2025-07-10 14:03:54 +0900

    Joins - INNER JOIN

--- 09-Joins.sql
+++ 09-Joins.sql
@@ -1,1 +1,8 @@
-SELECT * FROM dogs CROSS JOIN owners;
+SELECT
+  dogs.name AS dog_name,
+  owners.name AS owner_name,
+  breeds.name AS breed_name
+FROM
+  dogs
+  JOIN owners USING(owner_id)
+  JOIN breeds USING(breed_id);
