138c383a05905910fed28511a9d4db6e885d6b44
bioroid17 <trace1997@ajou.ac.kr>
2025-07-10 22:03:54 +0900

    Joins - OUTER JOIN

--- 09-Joins.sql
+++ 09-Joins.sql
@@ -1,8 +1,5 @@
 SELECT
   dogs.name AS dog_name,
-  owners.name AS owner_name,
-  breeds.name AS breed_name
+  owners.name AS owner_name
 FROM
-  dogs
-  JOIN owners USING(owner_id)
-  JOIN breeds USING(breed_id);
+  dogs RIGHT JOIN owners USING (owner_id);
