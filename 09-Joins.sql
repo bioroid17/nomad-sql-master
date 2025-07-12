ba7bc9f50cb736a087177f98ab3dcc678edc8ba7
bioroid17 <trace1997@ajou.ac.kr>
2025-07-12 19:11:23 +0900

    Joins - Joins Practice part 2

--- 09-Joins.sql
+++ 09-Joins.sql
@@ -1,56 +1,61 @@
--- 1. List all dogs with their breed names
+-- 1. Show all breeds and the count of dogs for each breed
 SELECT
-  dogs.name as dogs_name,
-  breeds.name as breeds_name
-FROM
-  dogs
-  LEFT JOIN breeds USING (breed_id);
-
--- 2. Show all owners and their dogs (if they have any)
-SELECT
-  owners.name as owners_name,
-  dogs.name as dogs_name
-FROM
-  owners
-  JOIN dogs USING (owner_id);
-
--- 3. Display all breeds and the dogs of that breed (if any)
-SELECT
-  breeds.name as breeds_name,
-  dogs.name as dogs_name
+  breeds.name AS breed_name,
+  COUNT(*) AS dog_count
 FROM
   breeds
-  JOIN dogs USING (breed_id);
+  RIGHT JOIN dogs USING (breed_id)
+GROUP BY
+  breed_name;
 
--- 4. List all dogs with their pet passport information and owner data (if avaliable)
+-- 2. Display all owners with the count of their dogs, the average dog weight and the average dog age.
 SELECT
-  d.name AS dogs_name,
-  pp.allergies,
-  pp.blood_type,
-  pp.lASt_checkup_date,
-  o.name AS owners_name
+  owners.name AS owner_name,
+  COUNT(dogs.dog_id) AS dog_count,
+  AVG(dogs.weight) AS avg_weight,
+  AVG(
+    TIMESTAMPDIFF(YEAR, dogs.date_of_birth, CURRENT_DATE)
+  ) AS avg_age
 FROM
-  dogs AS d
-  JOIN pet_passports AS pp USING (dog_id)
-  JOIN owners AS o USING (owner_id);
+  owners
+  LEFT JOIN dogs USING (owner_id)
+GROUP BY
+  owners.owner_id;
 
--- 5. Show all tricks and the dogs that know them
+-- 3. Show all tricks and the number of dogs that know each trick ordered by popularity
 SELECT
-  d.name as dogs_name,
-  t.name as tricks_name,
-  t.difficulty,
-  dt.proficiency,
-  dt.date_learned
+  t.name AS trick_name,
+  COUNT(dt.dog_id) AS total_dogs
 FROM
   tricks AS t
-  JOIN dog_tricks AS dt USING (trick_id)
-  JOIN dogs AS d USING (dog_id);
+  LEFT JOIN dog_tricks AS dt USING (trick_id)
+GROUP BY
+  t.trick_id
+ORDER BY
+  total_dogs DESC;
 
--- 6. Display all dogs that don't know a single trick
+-- 4. Display all dogs along with the count of tricks they know
 SELECT
-  d.name as dogs_name
+  d.name AS dog_name,
+  COUNT(dt.trick_id) AS total_tricks
 FROM
-  dog_tricks AS dt
-  RIGHT JOIN dogs AS d USING (dog_id)
-WHERE
-  dt.trick_id IS NULL;
+  dogs AS d
+  LEFT JOIN dog_tricks AS dt USING (dog_id)
+GROUP BY
+  d.dog_id
+ORDER BY
+  total_tricks DESC;
+
+-- 5. List all owner with their dogs and the tricks their dogs know
+SELECT
+  o.name owner_name,
+  d.name dog_name,
+  dt.proficiency,
+  t.name trick_name
+FROM
+  owners o
+  LEFT JOIN dogs d USING (owner_id)
+  LEFT JOIN dog_tricks dt USING (dog_id)
+  JOIN tricks t USING (trick_id)
+ORDER BY
+  o.name;
