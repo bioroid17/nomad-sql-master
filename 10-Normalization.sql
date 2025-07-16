a4241a4affd76d7cde973a99e24ec1839fa06bc9
bioroid17 <trace1997@ajou.ac.kr>
2025-07-17 01:02:56 +0900

    Normalization - Conclusions

--- 10-Normalization.sql
+++ 10-Normalization.sql
@@ -1,4 +1,3 @@
-DROP TABLE countries;
 CREATE TABLE countries (
   country_id BIGINT UNSIGNED PRIMARY KEY auto_increment,
   country_code CHAR(2) UNIQUE NOT NULL,
@@ -36,3 +35,57 @@ WHERE
   country LIKE '__,__'
 GROUP BY
   country;
+
+INSERT IGNORE INTO
+  countries (country_code)
+SELECT
+  SUBSTRING_INDEX(country, ',', 1)
+FROM
+  movies
+WHERE
+  country LIKE '__,__,__'
+GROUP BY
+  country
+UNION
+SELECT
+  SUBSTRING_INDEX(country, ',', -1)
+FROM
+  movies
+WHERE
+  country LIKE '__,__,__'
+GROUP BY
+  country
+UNION
+SELECT
+  SUBSTRING_INDEX(SUBSTRING_INDEX(country, ',', 2), ',', -1)
+FROM
+  movies
+WHERE
+  country LIKE '__,__,__'
+GROUP BY
+  country;
+
+CREATE TABLE movies_countries (
+  movie_id BIGINT UNSIGNED,
+  country_id BIGINT UNSIGNED,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (movie_id, country_id),
+  FOREIGN KEY (movie_id) REFERENCES movies (movie_id) ON DELETE CASCADE,
+  FOREIGN KEY (country_id) REFERENCES countries (country_id) ON DELETE CASCADE
+);
+
+INSERT INTO
+  movies_countries (movie_id, country_id)
+SELECT
+  movies.movie_id,
+  countries.country_id
+FROM
+  movies
+  JOIN countries ON movies.country LIKE CONCAT('%', countries.country_code, '%')
+WHERE
+  movies.country <> ''
+  AND countries.country_code <> '';
+
+ALTER TABLE movies
+DROP COLUMN country;
