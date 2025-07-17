b1967b3a2a3d568afc36ad3aec6aab185ccd939d
bioroid17 <trace1997@ajou.ac.kr>
2025-07-18 02:50:47 +0900

    Events & Triggers - Recap

--- 11-Events-and-Triggers.sql
+++ 11-Events-and-Triggers.sql
@@ -1,11 +1,8 @@
-CREATE TABLE archived_movies LIKE movies;
+SHOW events;
 
-DROP EVENT archive_old_movies;
+SHOW CREATE EVENT archive_old_movies;
 
-TRUNCATE TABLE archived_movies;
-
-DELIMITER $$
-CREATE EVENT archive_old_movies ON SCHEDULE EVERY 2 MINUTE STARTS CURRENT_TIMESTAMP + INTERVAL 2 MINUTE DO BEGIN
+CREATE DEFINER = `root` @`localhost` EVENT `archive_old_movies` ON SCHEDULE EVERY 2 MINUTE STARTS '2025-07-18 00:58:34' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
 INSERT INTO
   archived_movies
 SELECT
@@ -19,5 +16,4 @@ DELETE FROM movies
 WHERE
   release_date < YEAR(CURDATE()) - 20;
 
-END $$
-DELIMITER;
+END
