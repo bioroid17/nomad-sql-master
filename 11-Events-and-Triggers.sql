23ce8281d3f9c37b3768f5f1d65bd00569e008d8
bioroid17 <trace1997@ajou.ac.kr>
2025-07-18 22:51:48 +0900

    Events & Triggers - Overpowered Trigger

--- 11-Events-and-Triggers.sql
+++ 11-Events-and-Triggers.sql
@@ -1,61 +1,26 @@
 -- BEFORE: INSERT, UPDATE, DELETE
 -- AFTER: INSERT, UPDATE, DELETE
-CREATE TABLE records (
-  record_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  changes TINYTEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
+SHOW TRIGGERS;
 
-DROP TRIGGER before_movie_insert;
+DROP TRIGGER after_movie_update;
 
-CREATE TRIGGER before_movie_insert BEFORE INSERT ON movies FOR EACH ROW
-INSERT INTO
-  records (changes)
-VALUES
-  (CONCAT ('Will insert: ', NEW.title));
+TRUNCATE TABLE records;
 
-CREATE TRIGGER after_movie_insert AFTER INSERT ON movies FOR EACH ROW
-INSERT INTO
-  records (changes)
-VALUES
-  (CONCAT ('Insert completed: ', NEW.title));
+DELIMITER $$
+CREATE TRIGGER after_movie_update AFTER UPDATE
+ON movies FOR EACH ROW
+BEGIN
+  DECLARE changes TINYTEXT DEFAULT '';
 
-CREATE TRIGGER before_movie_update BEFORE
-UPDATE ON movies FOR EACH ROW
-INSERT INTO
-  records (changes)
-VALUES
-  (
-    CONCAT (
-      'Will update title: ',
-      OLD.title,
-      ' -> ',
-      NEW.title
-    )
-  );
+  IF NEW.title <> OLD.title THEN
+    SET changes = CONCAT(changes, 'Title changed ', OLD.title, ' -> ', NEW.title, '\n');
+  END IF;
 
-CREATE TRIGGER after_movie_update AFTER
-UPDATE ON movies FOR EACH ROW
-INSERT INTO
-  records (changes)
-VALUES
-  (
-    CONCAT (
-      'Update completed: ',
-      OLD.title,
-      ' -> ',
-      NEW.title
-    )
-  );
+  IF NEW.budget <> OLD.budget THEN
+    SET changes = CONCAT(changes, 'Budget changed ', OLD.budget, ' -> ', NEW.budget, '\n');
+  END IF;
+  
+  INSERT INTO records (changes) VALUES (changes);
 
-CREATE TRIGGER before_movie_delete BEFORE DELETE ON movies FOR EACH ROW
-INSERT INTO
-  records (changes)
-VALUES
-  (CONCAT ('Will delete title: ', OLD.title));
-
-CREATE TRIGGER after_movie_delete AFTER DELETE ON movies FOR EACH ROW
-INSERT INTO
-  records (changes)
-VALUES
-  (CONCAT ('Delete completed: ', OLD.title));
+END $$
+DELIMITER;
