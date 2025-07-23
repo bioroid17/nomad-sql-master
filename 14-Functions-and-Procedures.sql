b01697d55ce0bba00f0df319fd8e817663f4ae6a
bioroid17 <trace1997@ajou.ac.kr>
2025-07-23 17:55:10 +0900

    Functions and Procedures - Trigger Functions

--- 14-Functions-and-Procedures.sql
+++ 14-Functions-and-Procedures.sql
@@ -1,23 +1,4 @@
 CREATE
-OR REPLACE FUNCTION is_hit_or_flop (movie movies) RETURNS TEXT AS $$
-  SELECT
-  CASE
-    WHEN movie.revenue > movie.budget THEN 'Hit'
-    WHEN movie.revenue < movie.budget THEN 'Flop'
-    ELSE 'N/A'
-  END;
-$$ LANGUAGE SQL;
-
-SELECT
-  title,
-  is_hit_or_flop(movies.*)
-FROM
-  movies;
-
-DROP FUNCTION is_hit_or_flop(movies);
-
--- 여러 개의 값을 반환하는 함수
-CREATE
 OR REPLACE FUNCTION is_hit_or_flop (movie movies) RETURNS TABLE (hit_or_flop TEXT, other_thing NUMERIC) AS $$
   SELECT
   CASE
@@ -26,10 +7,24 @@ OR REPLACE FUNCTION is_hit_or_flop (movie movies) RETURNS TABLE (hit_or_flop TEX
     ELSE 'N/A'
   END,
   11111;
-$$ LANGUAGE SQL;
+$$ LANGUAGE SQL IMMUTABLE;
 
 SELECT
   title,
   (is_hit_or_flop(movies.*)).*
 FROM
   movies;
+
+-- updated_at 값 업데이트용 함수
+CREATE
+OR REPLACE FUNCTION set_updated_at () RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+  END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER updated_at BEFORE
+UPDATE -- OF title <- title 열에 대해서만 트리거 실행을 원할 경우
+ON movies FOR EACH ROW
+EXECUTE PROCEDURE set_updated_at ();
