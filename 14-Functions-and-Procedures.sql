b409882ce30461c9dd695e21cd6fe78ebe9dd66f
bioroid17 <trace1997@ajou.ac.kr>
2025-07-23 20:33:51 +0900

    Functions and Procedures - Procedures

--- 14-Functions-and-Procedures.sql
+++ 14-Functions-and-Procedures.sql
@@ -1,30 +1,43 @@
-CREATE
-OR REPLACE FUNCTION is_hit_or_flop (movie movies) RETURNS TABLE (hit_or_flop TEXT, other_thing NUMERIC) AS $$
-  SELECT
-  CASE
-    WHEN movie.revenue > movie.budget THEN 'Hit'
-    WHEN movie.revenue < movie.budget THEN 'Flop'
-    ELSE 'N/A'
-  END,
-  11111;
-$$ LANGUAGE SQL IMMUTABLE;
+-- return 하지 않는 프로시저
+CREATE PROCEDURE set_zero_revenue () AS $$
+  UPDATE movies SET revenue = NULL WHERE revenue = 0;
+$$ LANGUAGE SQL;
 
-SELECT
-  title,
-  (is_hit_or_flop(movies.*)).*
-FROM
-  movies;
+CALL set_zero_revenue ();
 
--- updated_at 값 업데이트용 함수
-CREATE
-OR REPLACE FUNCTION set_updated_at () RETURNS TRIGGER AS $$
+-- return 하는 프로시저
+CREATE PROCEDURE hello_world_p (IN name TEXT, OUT greeting TEXT) AS $$
   BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
+    greeting = 'Hello ' || name;
   END;
 $$ LANGUAGE PLPGSQL;
 
-CREATE TRIGGER updated_at BEFORE
-UPDATE -- OF title <- title 열에 대해서만 트리거 실행을 원할 경우
-ON movies FOR EACH ROW
-EXECUTE PROCEDURE set_updated_at ();
+-- CALL hello_world_p ('nico'); <- 에러
+CALL hello_world_p ('nico', NULL); -- 출력값에 해당하는 인자도 줘야 한다.
+
+-- PLPGSQL 맛보기
+-- 유저의 언어에 따라 여러 언어로 인사하는 함수 만들기
+CREATE PROCEDURE hello_world_i (IN name TEXT, IN lang TEXT, OUT greeting TEXT) AS $$
+  DECLARE
+    spanish_hello TEXT = 'hola';
+    italian_hello TEXT = 'ciao';
+    korean_hello TEXT = '안녕';
+  BEGIN
+    IF lang = 'korean' THEN
+      greeting := korean_hello || ' ' || name || '!';
+    ELSIF lang = 'italian' THEN
+      greeting := italian_hello || ' ' || name || '!';
+    ELSIF lang = 'spanish' THEN
+      greeting := spanish_hello || ' ' || name || '!';
+    ELSE
+      greeting := 'hello ' || name || '!';
+    END IF;
+  END;
+$$ LANGUAGE PLPGSQL;
+
+
+
+CALL hello_world_i('nico', 'korean', NULL);
+CALL hello_world_i('nico', 'italian', NULL);
+CALL hello_world_i('nico', 'spanish', NULL);
+CALL hello_world_i('nico', 'esperanto', NULL);
