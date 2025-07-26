12eff8a5f8142cb80bd1c4506db837ee1965cd8a
bioroid17 <trace1997@ajou.ac.kr>
2025-07-27 01:10:36 +0900

    Transactions - Shared Locks

--- 15-Transactions.sql
+++ 15-Transactions.sql
@@ -1,53 +1,31 @@
--- PostgreSQL에서 작성됨
-
-CREATE TABLE accounts (
-  account_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  account_holder VARCHAR(100) NOT NULL,
-  balance DECIMAL(10, 2) NOT NULL CHECK (balance >= 0)
-);
-
-DROP TABLE accounts;
-
-INSERT INTO
-  accounts (account_holder, balance)
-VALUES
-  ('nico', 1000.00),
-  ('lynn', 2000.00);
-
 -- 트랜잭션 A
 BEGIN;
 
-UPDATE accounts
-SET
-  balance = balance + 200
-WHERE
-  account_holder = 'lynn';
-
 SELECT
-  *
+  balance
 FROM
-  accounts;
-
-ROLLBACK;
+  accounts
+WHERE
+  account_holder = 'lynn'
+FOR UPDATE;
 
 COMMIT;
 
-
 -- 트랜잭션 B
-BEGIN;
-
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
-
--- do stuff
 UPDATE accounts
 SET
-  balance = balance - 100
+  account_holder = 'lynn'
 WHERE
-  account_holder = 'lynn';
+  account_holder = 'Lynn';
+
+BEGIN;
 
 SELECT
-  *
+  balance
 FROM
-  accounts;
+  accounts
+WHERE
+  account_holder = 'lynn'
+FOR SHARE;
 
 COMMIT;
