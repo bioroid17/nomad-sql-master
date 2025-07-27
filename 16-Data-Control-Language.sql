c7b82c3839126c5efeb1fb0dc8a843ad1b83d4f8
bioroid17 <trace1997@ajou.ac.kr>
2025-07-28 02:26:06 +0900

    Data Control Language - Roles

--- 16-Data-Control-Language.sql
+++ 16-Data-Control-Language.sql
@@ -1,33 +1,21 @@
--- 유저 생성
-CREATE ROLE marketer
-WITH
-  LOGIN PASSWORD 'marketing4ever';
+-- ROLE 생성(비밀번호 없음)
+CREATE ROLE editor;
 
--- 권한 부여
-GRANT
--- INSERT,
--- UPDATE,
-SELECT
-  ON movies TO marketer;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA "public" TO editor;
 
-GRANT
-SELECT, INSERT ON statuses, directors TO marketer;
+-- USER 생성(비밀번호와 함께 생성)
+CREATE USER editor_one WITH PASSWORD 'words4ever';
 
-GRANT
-SELECT
-  ON ALL TABLES IN SCHEMA public TO marketer;
+-- USER에게 ROLE 부여
+GRANT editor TO editor_one;
 
--- 권한 취소
-REVOKE INSERT ON statuses, directors FROM marketer;
+REVOKE ALL ON movies FROM editor;
 
-GRANT
-INSERT
-  ON ALL TABLES IN SCHEMA public TO marketer;
+-- 테이블의 특정 열에 대해서만 접근 허용
+GRANT SELECT (title) ON movies TO editor;
 
-GRANT
-SELECT, INSERT
-  ON ALL TABLES IN SCHEMA public TO marketer;
+GRANT UPDATE (budget) ON movies TO editor;
 
-REVOKE SELECT, INSERT ON ALL TABLES IN SCHEMA public
-FROM
-  marketer;
+-- 같은 유저와 비밀번호로 연결 못 하게 제한
+-- editor_one 연결은 1명까지만 가능
+ALTER ROLE editor_one WITH CONNECTION LIMIT 1;
