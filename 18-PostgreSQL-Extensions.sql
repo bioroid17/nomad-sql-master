28e07a6fd9d6231e285689f9c2691522c92e7c81
bioroid17 <trace1997@ajou.ac.kr>
2025-07-30 03:15:51 +0900

    PostgreSQL Extensions - PGCrypto

--- 18-PostgreSQL-Extensions.sql
+++ 18-PostgreSQL-Extensions.sql
@@ -1,73 +1,24 @@
-CREATE EXTENSION hstore;
--- DROP EXTENSION hstore;
+CREATE EXTENSION pgcrypto;
 
-SELECT
-  *
-FROM
-  pg_extension;
-
-CREATE TABLE users2 (
+CREATE TABLE users3 (
   user_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  prefs hstore -- hstore 타입
+  username VARCHAR(100),
+  password VARCHAR(100)
 );
 
--- hstore 값 삽입
+-- 비밀번호를 암호화해서 저장
+-- crypt(): 해시 함수
+-- gen_salt(): 해시 알고리즘 선택 함수
 INSERT INTO
-  users2 (prefs)
+  users3 (username, password)
 VALUES
-  ('theme => dark, lang => kr, notifications => off'),
-  (
-    'theme => light, lang => es, notifications => on, push_notifications => on, email_notifications => off'
-  ),
-  (
-    'theme => dark, lang => it, start_page => dashboard, font_size => large'
-  );
+  ('nico', crypt ('user_password', gen_salt ('bf')));
 
--- hstore 연산자
+-- 암호화된 비밀번호를 인증하는 방법
 SELECT
-  user_id,
-  prefs -> 'theme',
-  prefs -> ARRAY['lang', 'notifications'],
-  prefs ? 'font_size' AS has_font_size,
-  prefs ?| ARRAY['push_notifications', 'start_page']
+  username
 FROM
-  users2;
-
--- hstore 함수
-SELECT
-  user_id,
-  akeys(prefs),
-  avals(prefs)
-FROM
-  users2;
-
-SELECT
-  user_id,
-  each(prefs)
-FROM
-  users2;
-
--- 특정 행의 hstore 키-값 수정
-UPDATE users2
-SET
-  prefs['theme'] = 'light'
+  users3
 WHERE
-  user_id = 1;
-
--- 모든 행의 hstore에 새로운 키-값 추가
-UPDATE users2
-SET
-  prefs = prefs || hstore (
-    ARRAY['currency', 'cookies_ok'],
-    ARRAY['krw', 'yes']
-  );
-
--- 모든 행의 hstore에서 키 삭제
-UPDATE users2
-SET
-  prefs = delete (prefs, 'cookies_ok');
-
-SELECT
-  *
-FROM
-  users2;
+  username = 'nico'
+  AND password = crypt ('user_password', password);
