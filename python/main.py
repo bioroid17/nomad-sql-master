092712ec64a5fafd8443378f50288859a143fa77
bioroid17 <trace1997@ajou.ac.kr>
2025-08-05 00:57:53 +0900

    JavaScript and Python Drivers - Querying Data

--- python/main.py
+++ python/main.py
@@ -1,69 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db")
+conn = sqlite3.connect("../movies_download.db")
 
 cur = conn.cursor()
 
+res = cur.execute("SELECT movie_id, title FROM movies")
 
-def init_table():
-    cur.execute(
-        """
-    CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-    );
-"""
-    )
-    cur.execute(
-        """
-    INSERT INTO users (username, password) VALUES
-    ('nico', '123'), ('lynn', '321');
-"""
-    )
+# all_movies = res.fetchall()
 
+# first_20 = res.fetchmany(20)
+# next_20 = res.fetchmany(20)
 
-def print_all_users():
-    res = cur.execute("SELECT * FROM users;")
-    data = res.fetchall()
-    print(data)
+# print(res.fetchone(), res.fetchone(), res.fetchone(), res.fetchone(), res.fetchone())
 
-
-def i_change_password(username, new_password):
-    cur.execute(
-        f"UPDATE users SET password = '{new_password}' WHERE username = '{username}'"
-    )
-
-
-def s_change_password(username, new_password):
-    cur.execute(
-        "UPDATE users SET password = ? WHERE username = ?",
-        (new_password, username),
-    )
-
-
-data = [
-    ("lannna", 567),
-    ("bora", 123),
-    ("max", 123),
-    ("jja", 898),
-]
-
-# cur.executemany("INSERT INTO users (username, password) VALUES (?, ?)", data)
-
-data = [
-    {"name": "lannna", "password": 567},
-    {"name": "bora", "password": 123},
-    {"name": "max", "password": 123},
-    {"name": "jja", "password": 898},
-]
-
-cur.executemany(
-    "INSERT INTO users (username, password) VALUES (:name, :password)",
-    data,
-)
-
-print_all_users()
+for movie in res:
+    print(movie)
 
 conn.commit()
 conn.close()
