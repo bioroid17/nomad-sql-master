4a3ce369cb649e871962df5d6ea809c0a5be37fe
bioroid17 <trace1997@ajou.ac.kr>
2025-08-05 19:37:10 +0900

    JavaScript and Python Drivers - Redis Caching

--- python/main.py
+++ python/main.py
@@ -1,20 +1,27 @@
-import sqlite3
+import redis, sqlite3, json
+
+r = redis.Redis(host="localhost", port=6379, decode_responses=False)
 
 conn = sqlite3.connect("../movies_download.db")
-
 cur = conn.cursor()
 
-res = cur.execute("SELECT movie_id, title FROM movies")
 
-# all_movies = res.fetchall()
+def make_expensive_query():
+    redis_key = "director:movies"
+    cached_result = r.get(redis_key)
+    if cached_result:
+        print("Cache hit")
+        return json.loads(cached_result)
+    else:
+        print("Cache miss")
+        res = cur.execute("SELECT COUNT(*), director FROM movies GROUP BY director;")
+        all_rows = res.fetchall()
+        r.set(redis_key, json.dumps(all_rows), ex=20)
+        return all_rows
 
-# first_20 = res.fetchmany(20)
-# next_20 = res.fetchmany(20)
 
-# print(res.fetchone(), res.fetchone(), res.fetchone(), res.fetchone(), res.fetchone())
-
-for movie in res:
-    print(movie)
+v = make_expensive_query()
 
 conn.commit()
 conn.close()
+r.close()
